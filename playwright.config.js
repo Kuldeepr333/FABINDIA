4efef9e8c0a987fc72ccc7e7e7224e1a28170a7d
Kuldeep.Rana <kuldeep.Rana@qualitykiosk.com>
2026-07-25 00:12:51 +0530

    Fixed playwright config syntax error

--- playwright.config.js
+++ playwright.config.js
@@ -30,8 +30,10 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    headless: false,
-    trace: 'on-first-retry',
+   use: {
+  headless: true,
+  trace: 'on-first-retry',
+}
   },
 
   /* Configure projects for major browsers */
